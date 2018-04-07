f9ef93b8122a2eee156fdcb61dcc906c0a98a3db
Alexandre Doussot <doussot.alexandre@gmail.com>
2018-04-07 13:49:42 +0000

    Test for connexion support

--- server/serv.nim
+++ server/serv.nim
@@ -1,15 +1,48 @@
-import asyncnet, asyncdispatch
+import asyncnet, asyncdispatch, sequtils, strutils, locks
 
-var clients {.threadvar.}: seq[AsyncSocket]
+type
+    StateKind = enum Connected, Anonymous
+    State = ref object
+        case kind : StateKind:
+        of Connected: name : string
+        of Anonymous: nil
 
-proc process_client(client: AsyncSocket) {.async.} =
+type Client = tuple[state: State, sock:AsyncSocket]
+
+type delta_kind = enum ChangeName, None
+type delta = ref object
+    case kind: delta_kind
+    of ChangeName:
+        client: Client
+        name : string
+    of None: nil
+
+
+var clients {.threadvar.}: seq[Client]
+var clients_lock: Lock
+
+proc process_cmd(client: Client, cmd:seq[string]) : delta =
+     case cmd[0]:
+         of "CONNEXION":
+             echo("Incoming connexion")
+             result = delta(kind:ChangeName, client:client, name:cmd[1])
+         else:
+             echo "Unknown command"
+             result = delta(kind:None)
+
+
+proc process_client(client: Client) : Future[delta] {.async.} =
     while true:
-        let line = await client.recv_line()
-        if line.len == 0: break
-        for c in clients:
-            if c == client:
-                continue
-            await c.send(line & "\c\L")
+        let line = await client.sock.recv_line()
+        echo line
+        if line.len == 0:
+            echo("empty line")
+            break
+        let delta = client.process_cmd(line.split({'/'}))
+        if delta.kind == ChangeName:
+            echo("Change name!")
+        # Shouldn't return, but pass a message.
+        return delta
 
 proc serve() {.async.} =
     clients = @[]
@@ -19,10 +52,20 @@ proc serve() {.async.} =
     server.listen()
 
     while true:
-        let client = await server.accept()
+        let sock = await server.accept()
+        let client = Client((State(kind:Anonymous), sock))
         clients.add client
+        let delta = await process_client(client)
+        if delta.kind == ChangeName:
+            echo "go changed name"
+            # acquire clients_lock
+            clients.keepIf(proc (c:Client) : bool = c == delta.client)
+            # release clients_lock
+            for client in clients:
+                await client.sock.send(delta.name & "connected.")
+        else:
+            echo "got nothing"
 
-        async_check process_client(client)
 
 async_check serve()
 run_forever()
